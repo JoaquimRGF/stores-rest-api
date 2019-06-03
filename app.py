ae342ed91453ca54f43de01edb5e63c34469eacd
JoaquimRGF <joaquim.rgf@gmail.com>
2019-06-03 10:46:38 +0100

    Fixed not importing db when running the app.

--- app.py
+++ app.py
@@ -14,12 +14,6 @@ app.config['PROPAGATE_EXCEPTIONS'] = True
 app.secret_key = 'jose'
 api = Api(app)
 
-
-@app.before_first_request
-def create_table():
-    db.create_all()
-
-
 jwt = JWT(app, authenticate, identity)
 
 
